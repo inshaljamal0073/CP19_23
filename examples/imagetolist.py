7f1faaa8ea41cafc4b8ab79669a26cd5cf1e7ca8
zeeeshanIMRAN <sp19bsse0049@gmail.com>
2019-05-20 01:29:14 -0700

    this is to convert a image into list form

---
+++ examples/imagetolist.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+img = Image.open('img1.png').convert('L')  # convert image to 8-bit grayscale
+WIDTH, HEIGHT = img.size
+
+data = list(img.getdata()) # convert image data to a list of integers
+# convert that to 2D list (list of lists of integers)
+data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+
+# At this point the image's pixels are all in memory and can be accessed
+# individually using data[row][col].
+
+# For example:
+for row in data:
+    print(' '.join('{:3}'.format(value) for value in row))
+
+# Here's another more compact representation.
+chars = '@%#*+=-:. '  # Change as desired.
+scale = (len(chars)-1)/255.
+print()
+for row in data:
+    print(' '.join(chars[int(value*scale)] for value in row))
